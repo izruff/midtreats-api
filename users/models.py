a44349dea7674c8316925e6cd02c6f62a2d2dfb6
izruff <rafaelky.dev@gmail.com>
2024-05-24 03:26:23 +0800

    Added users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """
+    Represents a registered user.
+    """
+
+    username = models.CharField(max_length=30)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    preference = models.JSONField(default=dict)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
