70381e56b388dec12d9fb96d9f4c4520ceadf167
izruff <rafaelky.dev@gmail.com>
2024-05-24 14:38:44 +0800

    Minor changes to places service

--- geodata/services/__init__.py
+++ geodata/services/__init__.py
@@ -1,3 +1,4 @@
-from common.apis.maps import MapsClient
+from django.contrib.sessions.backends.db import SessionStore
 
-maps_client = MapsClient()
+session_store = SessionStore()
+session = session_store.load()
--- geodata/services/places.py
+++ geodata/services/places.py
@@ -1,3 +1,8 @@
+"""
+Services for handling places-related operations, using the Places API
+and session data to allow personalized results.
+"""
+
 from common.apis.maps import (
     MapsClient,
     MAX_RESTRICTION_RADIUS,
@@ -6,96 +11,91 @@ from common.apis.maps import (
 
 INITIAL_DISTANCE_THRESHOLD = 1000.0
 DEFAULT_RESULT_PER_PAGE = 20
+DEFAULT_PLACE_FIELDS = tuple()
 
-class PlacesService:
+maps_client = MapsClient()
+
+def _assign_preference_scores(
+    place_ids,
+    distance_rank=None,
+    popularity_rank=None,
+):
     """
-    A service class for handling places-related operations. Each
-    session is assigned a new instance to allow personalized results.
+    Returns a list of scores matching given list of place IDs based
+    on user preference, used for sorting a list of places.
+    
+    The scores are normalized to a scale of -1 to 1. A positive
+    score means the user prefers the place, while a negative score
+    means the opposite.
     """
+    pass
 
-    maps_client = MapsClient()
+def get_place_details(place_id, fields=DEFAULT_PLACE_FIELDS):
+    """
+    Returns information corresponding to the fields list about a place
+    given its ID.
+    """
+    pass
 
-    def __init__(self, session):
-        self.session = session
+def get_nearby_places_sorted(
+    location,
+    location_restriction=None,
+    max_result_count=None,
+):
+    """
+    Returns a list of places near the given location, sorted by
+    user preference.
+    
+    The results are combined from two API responses: one is ranked
+    by distance and the other by popularity. They are then filtered
+    and sorted by preference score.
 
-    def _assign_preference_scores(
-        self,
-        places,
-        distance_rank=None,
-        popularity_rank=None,
-    ):
-        """
-        Returns a list of scores matching given list of places based
-        on user preference, used for sorting a list of places.
-        
-        The scores are normalized to a scale of -1 to 1. A positive
-        score means the user prefers the place, while a negative score
-        means the opposite.
-        """
-        pass
+    Since the Places v2 API can only output at most 20 results per
+    request, the function will likewise return a limited number of
+    results. For more results, use the get_all_nearby_places function.
+    """
+    pass
 
-    def get_nearby_places_sorted(
-        self,
-        location,
-        location_restriction=None,
-        max_result_count=None,
-    ):
-        """
-        Returns a list of places near the given location, sorted by
-        user preference.
-        
-        The results are combined from two API responses: one is ranked
-        by distance and the other by popularity. They are then filtered
-        and sorted by preference score.
+def get_all_nearby_places_sorted(
+    location,
+    location_restriction=None,
+    page=1,
+    result_per_page=DEFAULT_RESULT_PER_PAGE,
+):
+    """
+    Returns a paginated list of places near the given location and
+    (mostly) sorted by user preference.
 
-        Since the Places v2 API can only output at most 20 results per
-        request, the function will likewise return a limited number of
-        results. For more results, use the get_all_nearby_places function.
-        """
-        pass
+    It works the same way as the get_nearby_places function, but
+    uses the old API to obtain more results. As such, the API
+    costs are significantly higher and it should be used sparingly.
+    """
+    pass
 
-    def get_all_nearby_places_sorted(
-        self,
-        location,
-        location_restriction=None,
-        page=1,
-        result_per_page=DEFAULT_RESULT_PER_PAGE,
-    ):
-        """
-        Returns a paginated list of places near the given location and
-        (mostly) sorted by user preference.
+def get_places_in_area_sorted(
+    location_restriction=None,
+    page=1,
+    result_per_page=DEFAULT_RESULT_PER_PAGE,
+):
+    """
+    Returns a paginated list of places in the given area and
+    (mostly) sorted by user preference.
 
-        It works the same way as the get_nearby_places function, but
-        uses the old API to obtain more results. As such, the API
-        costs are significantly higher and it should be used sparingly.
-        """
-        pass
+    The main distinction from get_all_nearby_places_sorted is that
+    the distance to a particular center is no longer a factor in
+    the ranking. This is useful for exploring places in a specific
+    area, such as a city or a country.
+    """
+    pass
 
-    def get_places_in_area_sorted(
-        self,
-        location_restriction=None,
-        page=1,
-        result_per_page=DEFAULT_RESULT_PER_PAGE,
-    ):
-        """
-        Returns a paginated list of places in the given area and
-        (mostly) sorted by user preference.
+def sort_places_by_preference(place_ids, filter=True):
+    """
+    Sorts (and optionally filters) a list of places by preference.
+    """
+    pass
 
-        The main distinction from get_all_nearby_places_sorted is that
-        the distance to a particular center is no longer a factor in
-        the ranking. This is useful for exploring places in a specific
-        area, such as a city or a country.
-        """
-        pass
-
-    def sort_places_by_preference(self, places, filter=True):
-        """
-        Sorts (and optionally filters) a list of places by preference.
-        """
-        pass
-
-    def filter_places_by_preference(self, places):
-        """
-        Filters a list of places by preference.
-        """
-        pass
+def filter_places_by_preference(place_ids):
+    """
+    Filters a list of places by preference.
+    """
+    pass
